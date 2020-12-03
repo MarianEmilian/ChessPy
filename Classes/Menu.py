5c6535ac5d540b66c897c82914be314f665f1d4e
Lila Marian <marianemilian99@gmail.com>
2020-12-03 20:00:42 +0200

    Main-Menu

--- Classes/Menu.py
+++ Classes/Menu.py
@@ -1,1 +1,40 @@
-#starting out
+import pygame, sys
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
+
+mainClock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption('Main Menu')
+screen = pygame.display.set_mode((500, 500), 0, 32, 0, 1)
+font = pygame.font.SysFont(None, 20)
+
+
+def main_menu():
+    while True:
+        # pune o imagine de background
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def main_menu():
+    while True:
+        # pune o imagine de background
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        pygame.display.update()
+        mainClock.tick(60)
+
+main_menu()
