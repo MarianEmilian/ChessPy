c50fb5068f27ca1e121073fdbbb174f9aaed8be7
Lila Marian <marianemilian99@gmail.com>
2020-12-04 20:47:46 +0200

    Further structering

---
+++ GameLogic/Directions.py
@@ -0,0 +1,6 @@
+from Game import Piece
+
+
+def get_directions(piece: Piece):
+    """Gets a piece, returns the directions it can move"""
+    if piece.n
--- Utils/Space.py
+++ Utils/Space.py
@@ -1,4 +1,1 @@
-class Point:
-    def __init__(self, line, column):
-        self.line = line
-        self.col = column
+
