e20a7924ba5eb1f6be1c38008ce4b49809945cb6
Lila Marian <marianemilian99@gmail.com>
2020-12-19 08:42:43 +0200

    Drawing a piece

--- Game/Piece.py
+++ Game/Piece.py
@@ -3,19 +3,42 @@ A piece is either a pawn, knight, bishop, rook, or queen.
 It belongs to a player or another, fact represented by its color.
 The piece has a place on the board represented by its row and column
 """
-from Utils.Constants import SQUARE_SIZE
+import os
+from Utils.Constants import SQUARE_SIZE, BOARD_BUFFER
+from Utils.Constants import IMG_PATH
+from pygame.image import load
+from pygame.transform import scale
 
 
 class Piece:
-    def __init__(self, name, color, row, col):
+    def __init__(self, name: str, color: str, row: int, col: int):
         self.name = name
         self.color = color
         self.row = row
         self.col = col
 
+        # the name of the images is the first letter of the color + the piece name
+        # Example: wPawn/bKing/bRook/wQueen
+        self.img_path = IMG_PATH + self.color[0] + name + ".png"
+
+        # variables for the position in space of the image
         self.x = 0
         self.y = 0
 
-    def calc_xy(self):
-        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
-        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+    def pawn_to_other(self, name: str):
+        # Updates pawn
+        self.name = name
+        self.img_path = IMG_PATH + self.color[0] + name + ".png"
+
+    def calc_coord(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2 + BOARD_BUFFER // 2 + 10
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2 + BOARD_BUFFER // 2 + 10
+
+    def draw(self, window):
+        piece_img = load(self.img_path)
+        # downsizing img. Rotate by 0 deg, multiply by 0,5
+        piece_img = scale(piece_img, (70, 70))
+        window.blit(piece_img, (self.x, self.y))
+
+    def to_string(self):
+        return self.color + self.name
