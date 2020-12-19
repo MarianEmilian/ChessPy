3b840ce360afa3d2b29b8153b72aa6b1d0efa276
Lila Marian <marianemilian99@gmail.com>
2020-12-19 08:43:10 +0200

    Put board

--- Menus/GameWindow.py
+++ Menus/GameWindow.py
@@ -1,16 +1,27 @@
 import pygame
 from Utils.Constants import WINDOW_HEIGHT, WINDOW_WIDTH
-from Game.Board import draw_board
+from Game.Board import *
+from Utils.Constants import BLACK
 
 pygame.display.set_caption('ChessPy')
 
 FPS = 60
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW.fill(BLACK)
+
+
+def init_board(board):
+    board.hardcode_pieces()
+    draw_board(WINDOW)
+    board.draw_pieces(WINDOW)
+    pygame.display.update()
 
 
 def game():
     run = True
     clock = pygame.time.Clock()
+    board = Board()
+    init_board(board)
     while run:
         clock.tick(FPS)
 
@@ -19,8 +30,6 @@ def game():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
-        draw_board(WINDOW)
-        pygame.display.update()
 
     pygame.quit()
 
